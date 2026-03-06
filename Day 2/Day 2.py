40c88b09a3a01b30f73e9f0d3514598592efbfe1
cheedella  Harshath <132482075+Harshath14@users.noreply.github.com>
2026-03-06 20:36:24 +0530

    Add files via upload

---
+++ Day 2/Day 2.py
@@ -0,0 +1,59 @@
+Student_Name  = "Cheedella Venkata Narayana Swamy Harshath"
+Student_ID = "CGH2906"
+Batch_No = "PFS-HYD-050"
+Email_ID = "cheedellaharshath4891@gmail.com"
+Date_of_Birth = "2002-02-24"
+Age = "23"
+Gender = "Male"
+Blood_Group = "B"
+City = "Guntur"
+State = "Andhra pradesh"
+Student_Phone_Number = "+916302215981"
+Parent_Phone_Number = "+916302082700"
+Github_Link = "https://github.com/Harshath14"
+
+College_Name = "centurion university of technology and management"
+USN_Number = 201801330014
+Qualification = "UG (Bachelor Degrees)"
+Department = "B.Tech - Computer Science (CSE, Cybersecurity, AI & ML, IoT, Cloud Computing, Data Analytics, Data Science)"
+
+Pass_out_Year = 2024
+Graduation_Percentage = "80%"
+Arrears = "No"
+No_of_Arrears = 0
+_10th_Pass_Year = 2018
+_10th_Percentage = "93%"
+_12th_Pass_Year = 2016
+_12th_Percentage = "90%"
+skills = ["Python", "Flask", "HTML", "CSS", "Bootstrap","Javascript","MySQL","Aws","Jenkins","Github","Linux","Terraform","Docker","Git"]
+
+print("-----------------personal_deatils----------------")
+print(f"StudentName = {Student_Name}")
+print(f"Student_ID = {Student_ID}")
+print(f"Batch_No   = {Batch_No}")
+print(f"Email_ID = {Email_ID}")
+print(f"Date_of_Birth = {Date_of_Birth}")
+print(f"Age = {Age }")
+print(f"Gender = {Gender }")
+print(f"Blood_Group = {Blood_Group}")
+print(f"City = {City}")
+print(f"State = {State}")
+print(f"Student_Phone_Number = {Student_Phone_Number}")
+print(f"Parent_Phone_Number = {Parent_Phone_Number}")
+print(f"Github_Link= {Github_Link}")
+
+print("---------------Educational_Details---------------")
+print(f"College_Name = {College_Name}")
+print(f"USN_Number = {USN_Number}")
+print(f"Qualification = {Qualification}")
+print(f"Department = {Department}")
+print(f"Pass_out_Year = {Pass_out_Year}")
+print(f"Graduation_Percentage = {Graduation_Percentage }")
+print(f"Arrears = {Arrears}")
+print(f"_10th_Pass Year = {_10th_Pass_Year}")
+print(f"_10th_Percentage = {_10th_Percentage}")
+print(f"_12th_Pass Year = {_12th_Pass_Year }")
+print(f"_12th_Percentage = {_12th_Percentage}")
+print(f"skills = {skills}")
+
+
