6d29fd6757dd63cb95aa70076c462d82b80a0d41
cheedella  Harshath <132482075+Harshath14@users.noreply.github.com>
2026-03-08 18:52:18 +0530

    Add files via upload

---
+++ Day 4/conditional statement.py
@@ -0,0 +1,129 @@
+'''
+simple if
+ex = 1
+min_balance = 5000
+curr_balance = 2000
+
+if curr_balance < min_balance:
+    print("send message and cut some amount")
+
+
+
+'''
+'''
+battery_percentage = 4
+
+if battery_percentage < 20:
+    print("your charging is low")
+    
+'''
+'''
+data = ('user@gmail.com' , 'user')
+mail = input("enter a mail")
+password = input("enetr a password")
+
+if data = (mail,password):
+    print("login successful")
+else:
+    print("invalid logins")
+
+
+'''
+'''
+code = False
+
+if code == True:
+    print("it is correct code")
+else:
+    print("it is invalid")
+'''
+'''
+fruits =['mango','grape','apple','pine apple']
+search_item = input("enter a item :")
+
+if search_item in fruits:
+    print(f"{search_item} found!  buy now")
+
+else:
+    print("out of stock")
+'''
+'''
+time = int(input("enter a hour: "))
+if 0 <= time <= 6:
+    print("apsrtc buses")
+elif 7 <= time <=12:
+    print("tgsrtc buses")
+elif 13 <= time <= 18:
+    print("apsrtc and tgsrtc")
+elif 19<= time <=24:
+    print(" private travels")
+else:
+    print("give the time in range 0 to 24")
+
+'''
+
+
+
+
+
+'''
+print("1. bike")
+print("2. auto")
+print("3. cab")
+
+choice = int(input("choose the option: "))
+if choice ==1 :
+    print("you booked bike successfully.\n driver is on the way")
+elif choice == 2:
+    print("hello! you booked successfully a auto.\n driver is on the way.")
+elif choice == 3:
+    print("you booked cab successfully a cab")
+    
+'''
+
+'''
+login_status = True
+premium_account = True
+
+if login_status:
+    print("welcome to the hotstar")
+    if premium_account:
+        print("play the video with high quality")
+    else:
+        print("play the video with normal quality")
+else:
+    print("invalid logins")
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
